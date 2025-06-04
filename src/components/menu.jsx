663de0abd3a2d442e042820945539a5fb7ee3604
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-06-04 13:59:35 -0300

    Update menu.jsx

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,6 +3,8 @@ import MenuImg from '../assets/menuIcon.png'
 import styles from './menu.module.css'
 import { useState } from "react";
 
+import IconMenu from '../assets/icons8-circled-right.gif'
+
 export const Menu = () => {
     const navigate = useNavigate();
     const [open, setOpen] = useState(false)
@@ -14,15 +16,34 @@ export const Menu = () => {
         navigate('/')
     }
 
-    return(
+    return (
         <nav className={open ? styles.navBar : styles.navBarClosed}>
-            <img src={MenuImg} alt="Menu Icon" onClick={() => setOpen(prev => !prev)}/>
-            <p onClick={goToDashboard}>Dashboard</p>
-            <p>Criar usuario</p>
-            <p onClick={goToUsers}>Lista de usuarios</p>
-            <p>Criar produto</p>
-            <p>Lista de produtos</p>
-            <p onClick={logout}>Sair</p>
+            <img className={styles.menu} src={MenuImg} alt="Menu Icon" onClick={() => setOpen(prev => !prev)} />
+            <div className={styles.wrapIconP}>
+                <img className={styles.iconMenu} src={IconMenu} alt="Icon menu" />
+                <p onClick={goToDashboard}>Dashboard</p>
+            </div>
+            <div className={styles.wrapIconP}>
+                <img className={styles.iconMenu} src={IconMenu} alt="Icon menu" />
+                <p>Criar usuario</p>
+            </div>
+            <div className={styles.wrapIconP}>
+                <img className={styles.iconMenu} src={IconMenu} alt="Icon menu" />
+                <p onClick={goToUsers}>Lista de usuarios</p>
+            </div>
+            <div className={styles.wrapIconP}>
+                <img className={styles.iconMenu} src={IconMenu} alt="Icon menu" />
+                <p>Criar produto</p>
+            </div>
+            <div className={styles.wrapIconP}>
+                <img className={styles.iconMenu} src={IconMenu} alt="Icon menu" />
+                <p>Lista de produtos</p>
+            </div>
+            <div className={styles.wrapIconP}>
+                <img className={styles.iconMenu} src={IconMenu} alt="Icon menu" />
+                <p onClick={logout}>Sair</p>
+            </div>
+
         </nav>
     )
 }
